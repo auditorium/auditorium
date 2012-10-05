8bfc47243ac3f5a3beb5fad35c8d23cd5285bf5a
Hendrik Sollich <Hendrik@hoodie.de>
2012-10-05 17:08:27 +0200

    added note for users who didn't sign up to a faculty

--- src/app/controllers/my_faculties_controller.rb
+++ src/app/controllers/my_faculties_controller.rb
@@ -3,7 +3,8 @@ class MyFacultiesController < ApplicationController
   #load_and_authorize_resource
 
   def index
-    @faculties = current_user.faculties_with_courses
+    @faculties = current_user.faculties
+    @faculties_with_courses = current_user.faculties_with_courses
 
   end
 end
