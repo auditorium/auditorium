53a44023d1066d55dedeff96f5925b95cbf68f54
Lars Beier <sn3ek@ymail.com>
2012-10-06 21:31:48 +0200

    Fixed search results permission problem. Users cannot see private posts if no permission.

--- src/app/controllers/search_controller.rb
+++ src/app/controllers/search_controller.rb
@@ -4,6 +4,7 @@ class SearchController < ApplicationController
     courses = Course.search(params[:query], match_mode: :any)
     
     posts = Post.search params[:query], match_mode: :any
+    posts.keep_if{|p| can? :read, p}
 
     @results = {:posts => posts, :courses => courses, :lectures => lectures}
     respond_to do |format|
