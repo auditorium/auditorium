0cd8326c61f6cc7340a1b6c617103b9c8c2dca4b
Lars Beier <sn3ek@ymail.com>
2012-10-06 23:17:29 +0200

    Set pagination to 20 per page

--- src/app/controllers/search_controller.rb
+++ src/app/controllers/search_controller.rb
@@ -1,13 +1,13 @@
 class SearchController < ApplicationController
   def index
     query = "*#{params[:query]}*"
-    lectures = Lecture.search(query, match_mode: :any).page(params[:lecture_page]).per(10)
-    courses = Course.search(query, match_mode: :any).page(params[:course_page]).per(10)
+    lectures = Lecture.search(query, match_mode: :any).page(params[:lecture_page]).per(20)
+    courses = Course.search(query, match_mode: :any).page(params[:course_page]).per(20)
     
     posts = Post.search query, match_mode: :any
     posts.keep_if{|p| can? :read, p}
 
-    Kaminari.paginate_array(posts).page(params[:post_page]).per(10)
+    Kaminari.paginate_array(posts).page(params[:post_page]).per(20)
 
     @results = {:posts => posts, :courses => courses, :lectures => lectures}
 
