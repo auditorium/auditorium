8adc236660bc49049ee8332803ec534d386b533e
Lars Beier <sn3ek@ymail.com>
2012-10-06 21:43:54 +0200

    Removed token

--- src/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Auditorium::Application.config.secret_token = 'aa11b759d98e8df3734f6aa381bbadcc29ed7d3c1b8cf22091e2ea0de95a30cf21c55d241e110f1723105789f5b038ec2acfacf8ea3f2c9f8870107502c22341'
