deec721fbee78f197d437c3e1eb175135d977228
Lars Beier <sn3ek@ymail.com>
2012-10-06 22:47:56 +0200

    Added pagination on search page

--- src/app/controllers/search_controller.rb
+++ src/app/controllers/search_controller.rb
@@ -1,13 +1,16 @@
 class SearchController < ApplicationController
   def index
     query = "*#{params[:query]}*"
-    lectures = Lecture.search(query, match_mode: :any)
-    courses = Course.search(query, match_mode: :any)
+    lectures = Lecture.search(query, match_mode: :any).page(params[:lecture_page]).per(10)
+    courses = Course.search(query, match_mode: :any).page(params[:course_page]).per(10)
     
     posts = Post.search query, match_mode: :any
     posts.keep_if{|p| can? :read, p}
 
+    Kaminari.paginate_array(posts).page(params[:post_page]).per(10)
+
     @results = {:posts => posts, :courses => courses, :lectures => lectures}
+
     respond_to do |format|
       format.js
       format.html
