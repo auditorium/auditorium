16fcad62b253745ab0f91a55633f2cec05605c20
Lars Beier <sn3ek@ymail.com>
2013-03-19 15:44:32 +0100

    Security update to rails 3.2.13. Removed capybara for now.

--- src/spec/spec_helper.rb
+++ src/spec/spec_helper.rb
@@ -9,7 +9,8 @@ Spork.prefork do
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
   require 'rspec/autorun'
-  require 'capybara/rspec'
+  #require 'capybara/rspec'
+  #require 'capybara/rails'
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
