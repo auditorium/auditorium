e83cbad436be0e6f504ca9d651ae95c480cbcb5e
Lars Beier <sn3ek@ymail.com>
2013-06-10 09:58:54 +0200

    Add attributes for current user in json object after login.

--- src/app/controllers/users/sessions_controller.rb
+++ src/app/controllers/users/sessions_controller.rb
@@ -11,7 +11,7 @@ class Users::SessionsController < Devise::SessionsController
         respond_with resource, :location => root_path
       end
       format.json do
-        render :json => { :response => 'ok', :auth_token => current_user.authentication_token }.to_json, :status => :ok
+        render :json => { username: current_user.username, first_name: current_user.first_name, last_name: current_user.last_name, email: current_user.email, :response => 'ok', :auth_token => current_user.authentication_token }.to_json, :status => :ok
       end
     end
   end
