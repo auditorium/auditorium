4675349c57cd46201e4ef361d35a339c83798a5f
Lars Beier <sn3ek@ymail.com>
2013-09-25 11:11:49 +0200

    Redirect to root url when not signed in.

--- src/app/controllers/home_controller.rb
+++ src/app/controllers/home_controller.rb
@@ -2,22 +2,25 @@ class HomeController < ApplicationController
   before_filter :signed_in?
 	
   def index
+    if signed_in?
+      @posts = Array.new  
 
-    @posts = Array.new  
-
-    announcements = Announcement.all unless params[:show_announcements] == 'no'
-    questions = Question.all unless params[:show_questions] == 'no'
-    topics = Topic.all unless params[:show_topics] == 'no'
+      announcements = Announcement.all unless params[:show_announcements] == 'no'
+      questions = Question.all unless params[:show_questions] == 'no'
+      topics = Topic.all unless params[:show_topics] == 'no'
 
 
 
-    @posts += announcements if announcements.present?
-    @posts += questions if questions.present?
-    @posts += topics if topics.present?
-    @posts = @posts.sort { |x,y| y.created_at <=> x.created_at }
+      @posts += announcements if announcements.present?
+      @posts += questions if questions.present?
+      @posts += topics if topics.present?
+      @posts = @posts.sort { |x,y| y.created_at <=> x.created_at }
 
-    @posts.keep_if { |p| p.subscribed?(current_user) } if params[:only_subscribed] == 'yes'
+      @posts.keep_if { |p| p.subscribed?(current_user) } if params[:only_subscribed] == 'yes'
 
-    respond_to :html #, :js
+      respond_to :html #, :js
+    else
+      redirect_to root_url
+    end
   end
 end
